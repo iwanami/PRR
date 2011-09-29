063f54f56ac5bd99cb88fcdc6ad92cb51e63b8ce
Numa Trezzini <Iwanami@Iwanami.local>
2011-09-29 09:51:18 +0200

    synch_thread v1

--- labo1/PRR_labo1/Master.java
+++ labo1/PRR_labo1/Master.java
@@ -17,35 +17,50 @@
    public Master(){
       Master.last_id++;
       
+      /**
+       * Creation du thread Maitre envoyant les messages de synchronisation
+       * aux esclaves. Creer les threads dans le constructeur permet non
+       * seulement d'avoir acces depuis les threads de communication aux
+       * informations contenues dans le maitre, mais aussi a ce que le maitre
+       * soit operationnel des sa creation.
+       */
       this.synch_thread = new Thread(){
          
          int current_id = 1;
          
-         Message mess;
-         
-         long master_time;
+         Message synch_mess;
+         Message follow_up_mess;
          
          public void run(){
             while(true){
+               //calcul du nouvel id. lorsque l'ID courant vaut last_id, il 
+               //passe a 0, qui est l'identifiant du maitre. nous evitons de
+               //lui envoyer un message en ajoutant 1. le range parcouru par
+               //current_id est donc [1;last_id]
                this.current_id = (this.current_id%Master.last_id)+1;
-               this.mess = Message.SYNC;
-               mess.setID(this.current_id);
-               this.master_time = System.nanoTime();
+               
+               //creation du message SYNC
+               this.synch_mess = Message.SYNC;
+               this.synch_mess.setID(this.current_id);
+               
+               //creation du message FOLLOW_UP
+               this.follow_up_mess = Message.FOLLOW_UP;
+               this.follow_up_mess.setID(this.current_id);
+               this.follow_up_mess.setTimeStamp(System.nanoTime());
+               
                //TODO diffusion message
-               this.mess = Message.FOLLOW_UP;
-               mess.setID(this.current_id);
-               mess.setTime(this.master_time);
+               
                //TODO diffusion message
+               
+               //attente avant la synchronisation du prochain esclave.
                try{
                   wait(5000);
                }
                catch(InterruptedException e){}
             }
-         }
-         
-         
-      }
+         } /*end run*/
+      }/*end synch_thread*/
      
-   }
+   }/*end Master*/
    
- }
+ }/*end Master class*/
